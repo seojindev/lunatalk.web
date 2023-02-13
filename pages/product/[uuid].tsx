c05e8b8fece00b092a505db63b7cd2901fd9c93e
kkwondev <kkwoncokr@gmail.com>
2023-02-13 22:58:28 +0900

    product detail tab add

--- pages/product/[uuid].tsx
+++ pages/product/[uuid].tsx
@@ -1,11 +1,19 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import Select from 'react-select';
+import { IoClose } from 'react-icons/io5';
+import { useState } from 'react';
+import cn from 'classnames';
 
 const Product: NextPage = () => {
+  const [selectTab, setSelectTab] = useState<string>('order');
+
+  const onChangeTab = (tabName: string) => {
+    setSelectTab(tabName);
+  };
   return (
-    <div className="flex flex-col">
-      <div className="grid grid-cols-2 tablet:grid-cols-1">
+    <div className="flex flex-col gap-8">
+      <div className="grid grid-cols-2 tablet:grid-cols-1 gap-3">
         <div className="flex justify-center w-[400px] mx-auto tablet:w-full tablet:max-w-[400px]">
           <Image
             src="https://media.lunatalk.co.kr/storage/products/rep/6cfbd8f340e67cf8791d7a638b91df80f4c2ef7e/S9971Db7IyaVGyOMKLa39apkrHFqNias1K62GX4T.jpg"
@@ -16,24 +24,35 @@ const Product: NextPage = () => {
           />
         </div>
         <div className="flex flex-col gap-3 flex-wrap">
-          <h2 className="text-[#010101] text-2xl">title</h2>
+          <h2 className="text-[#010101] text-2xl tablet:text-xl">title</h2>
           <div className="flex items-center gap-3">
-            <span className="text-black text-2xl">17000원</span>
-            <span className="text-[#8e8e8e] text-lg line-through">24000원</span>
+            <span className="text-black text-2xl tablet:text-lg">17000원</span>
+            <span className="text-[#8e8e8e] text-lg line-through tablet:text-base">
+              24000원
+            </span>
           </div>
-          <div className="text-[#a749ff] text-[15px]">
+          <div className="text-[#a749ff] text-[15px] tablet:text-sm">
             <p>1 리뷰</p>
           </div>
-          <div>
+          <div className="tablet:text-xs">
             <Select
+              id="selectBox"
+              instanceId="selectbox"
               options={[{ value: 'orange', label: 'Orange' }]}
               placeholder={'색상을 선택해주세요.'}
+              onChange={(e) => {
+                console.log(e);
+              }}
+              value={{ value: 'orange', label: 'Orange' }}
+              defaultValue={{ value: 'orange', label: 'Orange' }}
             />
           </div>
-          <div className="border-[1px] rounded p-3 flex flex-col gap-3">
-            <div className="flex justify-between">
+          <div className="border-[1px] rounded p-3 flex flex-col gap-3 tablet:text-sm">
+            <div className="flex justify-between ">
               <p>오렌지</p>
-              <button>X</button>
+              <button type="button">
+                <IoClose />
+              </button>
             </div>
             <div className="flex justify-between">
               <div>
@@ -43,11 +62,11 @@ const Product: NextPage = () => {
                   value={1}
                 />
               </div>
-              <div className="flex gap-3">
-                <button className="border-[1px] border-slate-600 rounded  text-center text-xl px-3 justify-center items-center">
+              <div className="flex gap-3 tablet:text-sm">
+                <button className="border-[1px] border-slate-600 rounded  text-center text-xl px-3 justify-center items-center  tablet:text-sm">
                   +
                 </button>
-                <button className="border-[1px] border-slate-400 rounded  text-center text-xl px-3">
+                <button className="border-[1px] border-slate-400 rounded  text-center text-xl px-3 tablet:text-sm">
                   -
                 </button>
               </div>
@@ -57,10 +76,42 @@ const Product: NextPage = () => {
             <p>총 상품 가격</p>
             <p>17450원</p>
           </div>
-          <div className="grid grid-cols-2 gap-3">
-            <button className="bg-[#a749ff] text-white py-3">구매하기</button>
-            <button className="bg-[#a749ff] text-white py-3">장바구니</button>
+          <div className="grid grid-cols-2 gap-3 tablet:text-sm">
+            <button className="bg-[#a749ff] text-white py-3 tablet:py-2">
+              구매하기
+            </button>
+            <button className="bg-[#a749ff] text-white py-3 tablet:py-2">
+              장바구니
+            </button>
           </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="grid grid-cols-3 px-40 text-center border-b-[1px] border-[#d7d7d7] text-xl font-semibold tracking-[-1.3px]">
+          <button
+            onClick={() => onChangeTab('order')}
+            className={cn('py-2 block text-[#666]', {
+              'text-black border-b-2 border-black': selectTab === 'order',
+            })}
+          >
+            상품 정보
+          </button>
+          <button
+            onClick={() => onChangeTab('product')}
+            className={cn('py-2 block text-[#666]', {
+              'text-black border-b-2 border-black': selectTab === 'product',
+            })}
+          >
+            주문 정보
+          </button>
+          <button
+            onClick={() => onChangeTab('review')}
+            className={cn('py-2 block text-[#666]', {
+              'text-black border-b-2 border-black': selectTab === 'review',
+            })}
+          >
+            리뷰
+          </button>
         </div>
       </div>
     </div>
