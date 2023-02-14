0994ed9b5f95ec43c0f3d3c60c71ed071146912e
kkwondev <kkwoncokr@gmail.com>
2023-02-14 23:00:07 +0900

    product publishing

--- pages/product/[uuid].tsx
+++ pages/product/[uuid].tsx
@@ -86,11 +86,11 @@ const Product: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col">
-        <div className="grid grid-cols-3 px-40 text-center border-b-[1px] border-[#d7d7d7] text-xl font-semibold tracking-[-1.3px]">
+      <div className="flex flex-col gap-10">
+        <div className="grid grid-cols-3 px-40 text-center border-b-[1px] border-[#d7d7d7] text-xl font-semibold tracking-[-1.3px] tablet:px-20 tablet:text-base tablet:gap-4 mobile:px-10 mobile:text-sm">
           <button
             onClick={() => onChangeTab('order')}
-            className={cn('py-2 block text-[#666]', {
+            className={cn('py-2 block text-[#666] tablet:py-1', {
               'text-black border-b-2 border-black': selectTab === 'order',
             })}
           >
@@ -98,7 +98,7 @@ const Product: NextPage = () => {
           </button>
           <button
             onClick={() => onChangeTab('product')}
-            className={cn('py-2 block text-[#666]', {
+            className={cn('py-2 block text-[#666] tablet:py-1', {
               'text-black border-b-2 border-black': selectTab === 'product',
             })}
           >
@@ -106,13 +106,56 @@ const Product: NextPage = () => {
           </button>
           <button
             onClick={() => onChangeTab('review')}
-            className={cn('py-2 block text-[#666]', {
+            className={cn('py-2 block text-[#666] tablet:py-1', {
               'text-black border-b-2 border-black': selectTab === 'review',
             })}
           >
             리뷰
           </button>
         </div>
+        {selectTab === 'order' && (
+          <div className="text-base tracking-[-1px] tablet:text-sm">
+            <p>
+              <strong>제조국</strong> : 한국 / <strong>제조사</strong> :
+              (주)서진산업 루나톡
+            </p>
+            <p>
+              <strong>취급 주의사항 : </strong>
+              <span>
+                물과 상처에 주의하세요. 때가 탔을경우 가벽게 부드러운 수건으로
+                닦아주세요.
+              </span>
+            </p>
+            <p>
+              <strong>품질보증기준 : </strong>
+              <span>
+                관련법 및 소비자분쟁해결 규정에 따르며 제품구입후 7일이내로
+                제품의 손상,훼손,변형 이 없는경우 교환 또는 환불이 가능하며
+                일주일 이내 사용중 정상상태에서 발생한 성능,기능상 하자 발생시
+                무상교환 또는 환급이 가능합니다.
+              </span>
+            </p>
+            <p>
+              <strong>A/S 책임자와 전화번호 : </strong>
+              <span>루나톡 고객센터 032-684-1565</span>
+            </p>
+          </div>
+        )}
+        {selectTab === 'product' && (
+          <div className="px-2 w-full">
+            <Image
+              src="https://media.lunatalk.co.kr/storage/products/detail/6cfbd8f340e67cf8791d7a638b91df80f4c2ef7e/4CdbQmT6yekK9MMB9JbTmkrwbD8dxuKTLAHaa1P9.jpg"
+              alt="상품"
+              width={1170}
+              height={30000}
+            />
+          </div>
+        )}
+        {selectTab === 'review' && (
+          <div className="text-center text-2xl font-semibold tracking-[-1px]">
+            <h2>리뷰가 존재하지 않습니다.</h2>
+          </div>
+        )}
       </div>
     </div>
   );
