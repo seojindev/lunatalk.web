e94d4fea7540f78b54cefc0348fb928709c3a48c
Kyoungwon Kang <73323028+kkwondev@users.noreply.github.com>
2023-02-20 07:29:49 +0000

    react hook form add

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,21 +1,49 @@
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 
+interface LoginForm {
+  loginId: string;
+  password: string;
+}
+
 function Login() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: 'onSubmit',
+    defaultValues: { loginId: '', password: '' },
+  });
+
   return (
     <div className="max-w-[700px] mx-auto p-16 border-[1px] border-[#ebebeb] rounded text-center flex flex-col gap-5 mobile:p-0 mobile:border-0 mobile:py-24">
-      <form className="flex flex-col gap-3">
+      <form
+        className="flex flex-col gap-3"
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
         <input
           className="border-[1px] border-[#ebebeb] p-4 mobile:text-sm mobile:p-3"
           type="text"
           placeholder="아이디"
+          {...register('loginId', { required: '아이디를 입력해주세요.' })}
         />
+        {errors.loginId && (
+          <p className="text-left text-sm text-red-500">
+            {errors.loginId.message}
+          </p>
+        )}
         <input
           className="border-[1px] border-[#ebebeb] p-4 mobile:text-sm mobile:p-3"
           type="password"
           placeholder="비밀번호"
+          {...register('password', { required: '비밀번호를 입력해주세요.' })}
         />
+        {errors.password && (
+          <p className="text-left text-sm text-red-500">
+            {errors.password.message}
+          </p>
+        )}
         <div className="flex flex-row gap-3 justify-end mb-3 text-sm">
           <Link href="/auth/register">회원가입</Link>
           <Link href="/auth/register">비밀번호 찾기</Link>
