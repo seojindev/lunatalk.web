0ca6e43dc8bf5f9e59dd74105af8af6d4ea42d98
kkwondev <kkwoncokr@gmail.com>
2023-05-13 18:15:23 +0900

    카테고리 버그 수정

--- components/category/hoc/CategoryHoc.tsx
+++ components/category/hoc/CategoryHoc.tsx
@@ -1,4 +1,5 @@
 import useCategoryDataQuery from '../../../hooks/query/useCategoryDataQuery';
+import _ from 'lodash';
 
 interface CategoryHocProps {
   WrappedComponent: React.ComponentType<any>;
@@ -13,7 +14,17 @@ function CategoryHoc(props: CategoryHocProps) {
     options,
     optionOnChange,
     selectedOption,
-    items: data?.products || [],
+    items: data?.products
+      ? _.compact(
+          _.map(data.products, (item) => {
+            if (item.rep_image.url) {
+              return item;
+            } else {
+              return null;
+            }
+          }),
+        )
+      : [],
   };
 
   return <WrappedComponent {...CategoryData} />;
