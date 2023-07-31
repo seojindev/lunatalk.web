f77fabf815030e45e6ade9684ca8a86833efceb0
Theo <kkw.theo@gmail.com>
2023-07-31 09:44:17 +0000

    next config modify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
   reactStrictMode: true,
   images: {
     domains: ['admin.lunatalk.co.kr', 'media.lunatalk.co.kr'],
